ae46c7ee920aba58e918a738292d777e275ba776
konstantinov <konstantnov@rosenergo.com>
2018-03-21 19:31:37 +0300

    travis push to docker repo

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build . --no-cache konstantinov90/shri_2018_nodejs_homework
+docker build --no-cache --tag konstantinov90/shri_2018_nodejs_homework .
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
 docker push konstantinov90/shri_2018_nodejs_homework
