9b9d5563469648f400f08c88edcf4bb1a9f46d42
konstantinov <konstantnov@rosenergo.com>
2018-03-22 21:41:43 +0300

    BAMBAMBAM===

--- docker_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# docker build --no-cache --tag konstantinov90/shri_2018_nodejs_homework .
-docker build --no-cache --tag registry.heroku.com/shri-2018-nodejs-homework-stag/web .
-docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
-docker push registry.heroku.com/shri-2018-nodejs-homework-stag/web
-# docker push konstantinov90/shri_2018_nodejs_homework
